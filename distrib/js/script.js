f2f5cd389484df7b323575433dcf69926cfefebb
Alex <manakov55@yandex.ru>
2017-12-22 17:37:02 +0700

    two sliders done

--- distrib/js/script.js
+++ distrib/js/script.js
@@ -1,4 +1,76 @@
 
+// табы из чего состоит лечение
+
+function openTab(tabNumber){ 
+    $(".treatment-content-tab").removeClass("treatment-content-tab_active");
+    $(".treatment-content-text__tab-contents").addClass("hidden");
+   
+    if (tabNumber == 1) {
+        $("#tab-contents-1").removeClass("hidden");
+        $("#treatment-tab-1").addClass("treatment-content-tab_active");
+    }
+    else if (tabNumber == 2) {
+        $("#tab-contents-2").removeClass("hidden");
+        $("#treatment-tab-2").addClass("treatment-content-tab_active");
+    }
+    else if (tabNumber == 3) {
+        $("#tab-contents-3").removeClass("hidden");
+        $("#treatment-tab-3").addClass("treatment-content-tab_active");
+    }    
+};
+
+// табы лечение по направлениям
+
+function openTab2(tabNumber){
+
+    // уберем у всех табов класс "активная" 
+    $(".course-tab").removeClass("course-tab_active");
+
+    // скроем все блоки содежримого табов
+    $(".treatment-course__tab-contents").addClass("hidden");
+   
+   // если нажат таб-1
+    if (tabNumber == 1) {
+        // покажем соответствующий блок контента
+        $("#treatment-course__tab-contents-1").removeClass("hidden");
+
+        // выделим соответствующий таб
+        $("#course-tab-1").addClass("course-tab_active");
+    }
+
+    else if (tabNumber == 2) { 
+        $("#treatment-course__tab-contents-2").removeClass("hidden");
+        $("#course-tab-2").addClass("course-tab_active");
+    }
+    else if (tabNumber == 3) { 
+        $("#treatment-course__tab-contents-3").removeClass("hidden");
+        $("#course-tab-3").addClass("course-tab_active");
+    }
+    else if (tabNumber == 4) { 
+        $("#treatment-course__tab-contents-4").removeClass("hidden");
+        $("#course-tab-4").addClass("course-tab_active");
+    }
+    else if (tabNumber == 5) { 
+        $("#treatment-course__tab-contents-5").removeClass("hidden");
+        $("#course-tab-5").addClass("course-tab_active");
+    }
+    else if (tabNumber == 6) { 
+        $("#treatment-course__tab-contents-6").removeClass("hidden");
+        $("#course-tab-6").addClass("course-tab_active");
+    }
+    else if (tabNumber == 7) { 
+        $("#treatment-course__tab-contents-7").removeClass("hidden");
+        $("#course-tab-7").addClass("course-tab_active");
+    }
+    else if (tabNumber == 8) { 
+        $("#treatment-course__tab-contents-8").removeClass("hidden");
+        $("#course-tab-8").addClass("course-tab_active");
+    }  
+};
+
+
+
+
 /* МОДАЛЬНЫЕ ОКНА */
 $(document).ready(function(){        
     $(".callback-link").on('click', function(){
